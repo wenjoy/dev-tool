873eb4d102c6e27349b1f59821cd4f0618021b18
George Xie <georgexie@George.local>
2021-07-13 15:18:56 +0800

    the key is pointer-events

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
 // import './inspector';
-console.log('debug');
+document.addEventListener('mouseover', (e) => {
+    const target = e.target
+    
+    const {
+        top,
+        left,
+        width,
+        height
+    } = (target as HTMLElement).getBoundingClientRect()
+
+    const hover = document.getElementById('selector')
+    hover.style.top = top+'px'
+    hover.style.left = left+'px'
+    hover.style.width = width+'px'
+    hover.style.height = height+'px'
+})
